648dd1ce60ef8faf660eb5349e90c51d60a0969e
Irina-Golubitsky <irina.golubitsky@gmail.com>
2021-07-21 13:25:59 -0700

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 let key = "29b980ef45f54cadca300e5a599d2a64";
-let city ="";
-let cityList=[];
+let city = "";
+let cityList = [];
+// function for serch button on click
 function GetWeather() {
     city = $('#city').val();
     $('#city').val("");
-    displayCityWeather(city,1);
-    
+    displayCityWeather(city, 1);
+
 }
-let displayCityWeather = function (city,newSearch) {
+//  function display weather parameters: city name and boolean newSearch (add new city button or not)
+let displayCityWeather = function (city, newSearch) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
     console.log(apiUrl);
     fetch(apiUrl)
@@ -15,10 +17,14 @@ let displayCityWeather = function (city,newSearch) {
             // request was successful
             if (response.ok) {
                 response.json().then(function (data) {
+                    // show current weather
                     $('#city-weather').empty();
                     let h = $('<h4>');
-                    city=data.name;
-                    h.text(city + ' (' + moment.unix(data.dt + data.timezone).format("L") + ') '); //??? local date
+                    city = data.name;
+                    h.text(city + ' (' + moment.unix(data.dt + data.timezone).format() + ') ');
+                    console.log(city);
+                    console.log(data.dt);
+                    console.log(data.timezone);
                     let icon = $('<img>');
                     icon.attr("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
                     icon.attr("width", 50);
@@ -32,10 +38,13 @@ let displayCityWeather = function (city,newSearch) {
                     pHum.text("Humidity: " + data.main.humidity + " %");
                     $('#city-weather').append(pHum);
                     $('#city-weather').addClass('bg-white');
+                    // new fetch to get UV Index
                     var apiURLuv = `https://api.openweathermap.org/data/2.5/uvi?appid=${key}&lat=${data.coord.lat}&lon=${data.coord.lon}`;
                     fetch(apiURLuv)
                         .then(function (response) {
                             response.json().then(function (dataUv) {
+                                //show UV Index
+                                console.log(data);
                                 pUv = $('<p>');
                                 pUv.text("UV Index: ");
                                 let sUv = $('<span>');
@@ -56,10 +65,11 @@ let displayCityWeather = function (city,newSearch) {
                                 pUv.append(sUv);
                                 $('#city-weather').append(pUv);
                                 displayForecat(city);
-                                if (newSearch){
+                                // add new city button if needed
+                                if (newSearch) {
                                     cityList.push(city);
                                     localStorage.setItem('city-list', JSON.stringify(cityList));
-                                addCityButton(city);
+                                    addCityButton(city);
                                 }
                             });
                         })
@@ -68,6 +78,7 @@ let displayCityWeather = function (city,newSearch) {
                         });
                 });
             } else {
+                // show "city not found" if respose 404
                 $('#forecast').empty();
                 $('#forecast').removeClass("bg-white");
                 $('#city-weather').empty();
@@ -82,43 +93,48 @@ let displayCityWeather = function (city,newSearch) {
 
 
 }
+// function for 5-day forecast
 let displayForecat = function (city) {
     var apiURLForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`
 
     fetch(apiURLForecast)
         .then(function (response) {
             response.json().then(function (data) {
+                console.log("5 days");
                 console.log(data);
                 let h4 = $('<h4>');
-                h4.text("5-Day Forecast: 3 pm");
+                h4.text("5-Day Forecast:");
 
                 $('#forecast').empty();
                 $('#forecast').addClass("border-top border-3 p-3 bg-white");
                 $('#forecast').append(h4);
                 let mainDiv = $('<div>');
                 mainDiv.addClass("card-group d-flex flex-wrap");
-                for (i = 0; i < data.list.length; i++) {
-                    if ((moment(data.list[i].dt_txt)).toString().indexOf("15:00:00") > 0) {
-                        let div = $('<div>');
-                        div.addClass("card bg-forecast text-light m-2 p-2");
-                        let h5 = $('<h5>');
-                        h5.text(moment(data.list[i].dt_txt).format("L"));
-                        let icon = $('<img>');
-                        icon.addClass("mx-auto");
-                        icon.attr("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png");
-                        icon.attr("width", 75);
-                        div.append(h5);
-                        div.append(icon);
-                        let pTemp = $('<p>'), pWind = $('<p>'), pHum = $('<p>');
-                        pTemp.text("Temp: " + data.list[i].main.temp + " °F");
-                        div.append(pTemp);
-                        pWind.text("Wind: " + data.list[i].wind.speed + " MPH");
-                        div.append(pWind);
-                        pHum.text("Humidity: " + data.list[i].main.humidity + " %");
-                        div.append(pHum);
-                        mainDiv.append(div);
-                        $('#forecast').append(mainDiv);
-                    }
+                // calculate timezone difference to choose day hour for the forecast
+                timeIndex = 4 - parseInt(data.city.timezone / (3600 * 3));
+                console.log(timeIndex);
+                // show forecast for each day
+                for (i = timeIndex; i < data.list.length; i += 8) {
+                   let div = $('<div>');
+                    div.addClass("card bg-forecast text-light m-2 p-2");
+                    let h5 = $('<h5>');
+                    h5.text(moment(data.list[i].dt_txt).format("L"));
+                    let icon = $('<img>');
+                    icon.addClass("mx-auto");
+                    icon.attr("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png");
+                    icon.attr("width", 75);
+                    div.append(h5);
+                    div.append(icon);
+                    let pTemp = $('<p>'), pWind = $('<p>'), pHum = $('<p>');
+                    pTemp.text("Temp: " + data.list[i].main.temp + " °F");
+                    div.append(pTemp);
+                    pWind.text("Wind: " + data.list[i].wind.speed + " MPH");
+                    div.append(pWind);
+                    pHum.text("Humidity: " + data.list[i].main.humidity + " %");
+                    div.append(pHum);
+                    mainDiv.append(div);
+                    $('#forecast').append(mainDiv);
+
                 }
             });
         })
@@ -126,9 +142,11 @@ let displayForecat = function (city) {
             alert("Unable to connect to openweathermap.org");
         });
 }
+// add city button to search history
 let addCityButton = function (city) {
-    if (cityList.length === 1){
-        
+    // add emptyty div with border after search button
+    if (cityList.length === 1) {
+
         lineDiv = $('<div>');
         lineDiv.addClass("border-bottom border-2 mt-2");
         $('#first-row').append(lineDiv);
@@ -136,17 +154,19 @@ let addCityButton = function (city) {
     cityButton = $('<button>');
     cityButton.addClass('btn btn-secondary mt-2 mb-2 btn-block');
     cityButton.text(city);
+    // add on click function to show weather
     cityButton.on('click', function (event) {
         event.preventDefault();
-        displayCityWeather(city,0);})
+        displayCityWeather(city, 0);
+    })
     $('#first-row').append(cityButton);
 }
 
-
+// show search history buttons 
 cityList = JSON.parse(localStorage.getItem('city-list')) || [];
-    for (i=0;i<cityList.length;i++){
-        addCityButton(cityList[i]);
-    }
+for (i = 0; i < cityList.length; i++) {
+    addCityButton(cityList[i]);
+}
 
-
+//search button onclick function
 $('#search').click(GetWeather);
